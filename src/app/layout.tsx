ddf502e84ae8c08caf2385b2da378dfcc913faef
sachinkumar9849 <englishjha@gmail.com>
2025-07-07 23:50:51 +0545

    Nextjs font add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,15 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto } from 'next/font/google';
 import "./globals.css";
 import Nav from "./component/header/Nav";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const roboto = Roboto({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-roboto',
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,9 +24,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={roboto.className}
       >
-        <Nav/>
+        <Nav />
         {children}
       </body>
     </html>
