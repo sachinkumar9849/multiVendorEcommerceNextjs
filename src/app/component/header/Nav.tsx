9fa53a05dd9df736c4f53108af27317ca49aeeb4
sachinkumar9849 <englishjha@gmail.com>
2025-07-07 23:39:59 +0545

    project setup

---
+++ src/app/component/header/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopHeader from './TopHeader'
+
+const Nav = () => {
+  return (
+    <div>
+        <TopHeader/>
+    </div>
+  )
+}
+
+export default Nav
---
+++ src/app/component/header/TopHeader.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TopHeader = () => {
+    return (
+        <div className='bg-[#222f3e]'>
+            <div className="mx-auto max-w-7xl md:px-0 px-4">
+                <p className='text-[12px] text-white'> <b>Welcome to Emarket !</b> Wrap new offers / gift every single day on Weekends – New Coupon code: Happy2025 </p>
+<div className="">
+    
+</div>
+            </div>
+        </div>
+    )
+}
+
+export default TopHeader
