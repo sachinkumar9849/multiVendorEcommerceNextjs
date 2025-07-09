ad31aa34d314a68c20d507f1b2dfd120d4221de6
sachinkumar9849 <englishjha@gmail.com>
2025-07-09 07:10:42 +0545

    Home page banner slider done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import Image from "next/image";
 
+import CategoryMenuHome from "./component/Home/CategoryMenuHome";
+import HomePageBanner from "./component/Home/HomePageBanner";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-     <p>Body</p>
+    <div className="mx-auto max-w-7xl md:px-0 px-4">
+      <div className="grid grid-cols-10 gap-6">
+      <div className="col-span-2">
+        <CategoryMenuHome/>
+      </div>
+      <div className="col-span-6 mt-4">
+        <HomePageBanner/>
+      </div>
+     
+    </div>
     </div>
   );
 }
